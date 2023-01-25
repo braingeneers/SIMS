509b6d7c595a88b40ab2594998c9012655a29279
Julian Lehrer <jmlehrer@ucsc.edu>
2023-01-24 16:23:16 -0800

    fixed init

--- scsims/__init__.py
+++ scsims/__init__.py
@@ -2,5 +2,5 @@ from .data import *
 from .inference import MatrixDatasetWithLabelsFile, MatrixDatasetWithoutLabels
 from .lightning_train import *
 from .model import *
-from .networking import UploadCallback, upload
+from .networking import UploadCallback
 from .scvi_api import SIMS

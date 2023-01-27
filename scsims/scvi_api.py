fb389ba6d4ad5774a72f61bbffa49e6205b39bb3
Julian Lehrer <jmlehrer@ucsc.edu>
2023-01-26 17:57:13 -0800

    Verbosity

--- scsims/scvi_api.py
+++ scsims/scvi_api.py
@@ -17,6 +17,7 @@ class SIMS:
         *args,
         **kwargs,
     ) -> None:
+        print('Setting up data module ...')
         self.datamodule = DataModule(
             datafiles=[datafiles] if isinstance(datafiles, an.AnnData) else datafiles, 
             # since datamodule expects a list of data always
@@ -28,12 +29,15 @@ class SIMS:
             setattr(self, att, value)
 
     def setup_model(self, *args, **kwargs):
+        print('Setting up model ...')
         self._model = SIMSClassifier(self.datamodule.input_dim, self.datamodule.output_dim, *args, **kwargs)
 
     def setup_trainer(self, *args, **kwargs):
+        print('Setting up trainer ...')
         self._trainer = pl.Trainer(*args, **kwargs)
 
     def train(self, *args, **kwargs):
+        print('Beginning training')
         if not hasattr(self, "_trainer"):
             self.setup_trainer(*args, **kwargs)
         if not hasattr(self, "_model"):
@@ -41,13 +45,18 @@ class SIMS:
 
         self._trainer.fit(self._model, datamodule=self.datamodule)
 
+        print('Finished training')
+
     def predict(self, datafiles: an.AnnData, *args, **kwargs):
+        print('Beginnging prediction ...')
         results = self._model.predict(datafiles, *args, **kwargs)
         results = results.apply(lambda x: self.label_encoder(x))
 
+        print('Finished prediction, returning results ...')
         return results
 
     def explain(self, datafiles: an.AnnData, *args, **kwargs):
+        print('Computing explainability matrix ...')
         results = self._model.explain(datafiles, *args, **kwargs)
         
         return results

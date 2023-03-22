dfc9abb68c1be62b850fad58d4c89335b1e6fe66
Julian Lehrer <jmlehrer@ucsc.edu>
2023-03-22 09:56:16 -0700

    Verisioned torchmetrics correctly

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requirements = [
     "scanpy==1.9.1",
     "anndata==0.8.0",
     "pytorch_tabnet==3.1.1",
-    "torchmetrics==0.10.3",
+    "torchmetrics==0.8.2",
     "scikit-learn==1.0.2",
     "scipy==1.8.0",
     "wandb==0.12.16",

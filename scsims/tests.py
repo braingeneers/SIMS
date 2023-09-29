d8ac7b9ec8452816143e1d63484af33928c9e3b6
Julian Lehrer <jmlehrer@ucsc.edu>
2023-09-29 12:45:31 -0700

    better tests

--- scsims/tests.py
+++ scsims/tests.py
@@ -3,6 +3,7 @@ from scsims import SIMS
 import numpy as np
 import os 
 import pytorch_lightning as pl 
+import shutil
 
 def test_inference_more_genes_than_train():
     traindata = blobs(n_variables=10, n_observations=100)
@@ -13,9 +14,15 @@ def test_inference_more_genes_than_train():
     testdata.X = testdata.X.astype(np.float32)
 
     model = SIMS(data=traindata, class_label="blobs")
-    model.train(max_epochs=2)
+    model.setup_trainer(callbacks=[pl.callbacks.ModelCheckpoint(dirpath="gene_removal_test_checkpoint")], max_epochs=2)
+    model.train()
+
+    checkpoint_path = os.path.join("gene_removal_test_checkpoint", os.listdir("gene_removal_test_checkpoint")[0])
+    model = SIMS(weights_path=checkpoint_path)
 
     predictions = model.predict(testdata)
+
+    shutil.rmtree(os.path.dirname(checkpoint_path))
     assert len(predictions) == 100
     
 def test_inference_less_genes_than_train():
@@ -28,9 +35,14 @@ def test_inference_less_genes_than_train():
     testdata.X = testdata.X.astype(np.float32)
 
     model = SIMS(data=traindata, class_label="blobs")
-    model.train(max_epochs=2)
+    model.setup_trainer(callbacks=[pl.callbacks.ModelCheckpoint(dirpath="zero_inflation_test_checkpoint")], max_epochs=2)
+    model.train()
 
+    checkpoint_path = os.path.join("zero_inflation_test_checkpoint", os.listdir("zero_inflation_test_checkpoint")[0])
+    model = SIMS(weights_path=checkpoint_path)
     predictions = model.predict(testdata, num_workers=0)
+    shutil.rmtree(os.path.dirname(checkpoint_path))
+
     assert len(predictions) == 100
 
 def test_inference_decoded():
@@ -50,6 +62,8 @@ def test_inference_decoded():
     predictions = model.predict(testdata, num_workers=0)
 
     predicted_labels = predictions["first_pred"].unique()
+    # remove test checkpoint
+    shutil.rmtree(os.path.dirname(checkpoint_path))
     assert all(x.startswith("label_") for x in predicted_labels)
 
 if __name__ == "__main__":

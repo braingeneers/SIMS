0fbf7f79587901e8f475068800f6a723c0afa337
Julian Lehrer <jmlehrer@ucsc.edu>
2024-03-13 09:48:03 -0700

    UPdate networking file

--- scsims/networking.py
+++ scsims/networking.py
@@ -3,58 +3,66 @@ from typing import *
 
 import boto3
 import pytorch_lightning as pl
-
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 class UploadCallback(pl.callbacks.Callback):
-    """Custom PyTorch callback for uploading model checkpoints to a S3 bucket.
+    """Custom PyTorch callback for uploading the best model checkpoint to a S3 bucket.
 
     Parameters:
     path: Local path to folder where model checkpoints are saved
     desc: Description of checkpoint that is appended to checkpoint file name on save
-    upload_prefix: Path in bucket/ to upload model checkpoints to, defaults to model_checkpoints
+    s3: boto3.resource instance
+    bucket: Name of the S3 bucket
+    upload_prefix: Path in bucket to upload model checkpoints to, defaults to model_checkpoints
+    metric: Metric to monitor for checkpointing
+    mode: 'max' or 'min' depending on whether higher values of the metric are better or worse
     """
 
     def __init__(
         self,
-        path: str,
         desc: str,
-        s3: boto3.resource,
-        bucket: str,
-        upload_prefix="model_checkpoints",
-        n_epochs: int = 10,
-        quiet: bool = False,
+        path: str = "model_checkpoints",
+        upload_prefix = "model_checkpoints",
+        metric: str = 'val_micro_accuracy',
+        mode: str = 'max',
     ) -> None:
         super().__init__()
         self.path = path
         self.desc = desc
 
-        self.s3 = s3
-        self.bucket = bucket
+        self.s3 = boto3.resource(
+            "s3",
+            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+            aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+            endpoint_url="https://s3-west.nrp-nautilus.io",
+        )
+        self.bucket = "braingeneersdev"
         self.upload_prefix = upload_prefix
-        self.epochs = n_epochs
-        self.quiet = quiet
+        self.metric = metric
+        self.mode = mode
 
-    def on_train_epoch_end(self, trainer, pl_module):
-        epoch = trainer.current_epoch
+        # Initialize ModelCheckpoint callback
+        self.checkpoint_callback = ModelCheckpoint(
+            dirpath=self.path,
+            filename=f"best_{self.metric}_{self.desc}",
+            monitor=self.metric,
+            mode=self.mode,
+            save_top_k=1,  # Save only the best checkpoint
+            verbose=False,
+        )
 
-        if epoch % self.epochs == 0:  # Save every ten epochs
-            checkpoint = f"checkpoint-{epoch}-desc-{self.desc}.ckpt"
-            checkpoint_path = os.path.normpath(os.path.join(self.path, checkpoint))
+    def on_validation_end(self, trainer, pl_module):
+        # Call the ModelCheckpoint callback's on_validation_end method
+        self.checkpoint_callback.on_validation_end(trainer, pl_module)
 
-            if not self.quiet:
-                print(f"Saving checkpoint on epoch {epoch} to {checkpoint_path}")
-
-            trainer.save_checkpoint(checkpoint_path)
-
-            if not self.quiet:
-                print(f"Uploading checkpoint at epoch {epoch}")
-
-            try:
-                self.s3.Bucket(self.bucket).upload_file(
-                    Filename=checkpoint_path,
-                    Key=os.path.join(self.upload_prefix, checkpoint_path),
-                )
-
-            except Exception as e:
-                print(f"Error when uploading on epoch {epoch}")
-                print(e)
+    def on_train_end(self, trainer, pl_module):
+        # Upload the best checkpoint to S3
+        best_checkpoint_path = os.path.join(self.path, self.checkpoint_callback.best_model_path)
+        try:
+            self.s3.Bucket(self.bucket).upload_file(
+                Filename=best_checkpoint_path,
+                Key=os.path.join(self.upload_prefix, os.path.basename(best_checkpoint_path)),
+            )
+            print(f"Uploaded best checkpoint to S3: {best_checkpoint_path}")
+        except Exception as e:
+            print(f"Error when uploading the best checkpoint to S3: {e}")

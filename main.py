a44a0277542a86cb8e01de83067529107cce336b
JesusGF1 <jesusgzlezferrer@gmail.com>
2024-10-08 14:19:02 -0700

    Added zero inflation test to the set

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ import scanpy as sc
 from scsims import SIMS 
 import torch
 
-device = "cuda" if torch.cuda.is_available() else None
-dataset = sc.datasets.pbmc3k_processed()
 
-sims = SIMS(data=dataset, class_label="louvain")
-sims.setup_trainer(devices=1, max_epochs=10, accelerator="gpu" if device else "cpu")
-sims.train()
+def main():
+    device = "cuda" if torch.cuda.is_available() else None
+    dataset = sc.datasets.pbmc3k_processed()
 
-predictions = sims.predict(dataset)
+    sims = SIMS(data=dataset, class_label="louvain")
+    sims.setup_trainer(devices=1, max_epochs=2, accelerator="gpu" if device else "cpu")
+    sims.train()
+
+    #Remove 100 genes from the dataset
+    print(dataset.shape)
+    dataset = dataset[:, 100:]
+    print(dataset.shape)
+    predictions = sims.predict(dataset)
+    print(predictions)
+
+if __name__ == '__main__':
+    import multiprocessing
+    multiprocessing.freeze_support()
+    main()
